99864e31cfc81a46698845450117b6c1099de675
Andreas Hadjigavriel <74880624+AndreasHadjigavriel@users.noreply.github.com>
2021-04-18 12:28:55 +0300

    Initialize Customer Class

---
+++ src/main/java/Classes/Customer.java
@@ -0,0 +1,21 @@
+
+package Classes;
+
+
+public class Customer {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String member;
+    
+    public Customer(String name, String surname,String email, String password, String member){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.member = member;
+    }
+}
+
+    
